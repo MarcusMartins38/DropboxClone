c68a554dc38603d9c456e9709ac2aa3a6d9eeb96
Marcus Martins <marcusmartins38@gmail.com>
2020-08-26 15:24:58 -0400

    Created Sections

--- src/components/Section/styles.ts
+++ src/components/Section/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { FaDropbox } from "react-icons/fa";
 
 export const Container = styled.div`
   --padding-top: 100px;
@@ -39,6 +40,86 @@ export const Container = styled.div`
       --heading-font-size: 71px;
     }
   }
+
+  background: var(--bg-color);
+  position: relative;
 `;
 
-export const Content = styled.div``;
+export const HeaderWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+
+  clip: rect(auto, auto, auto, auto);
+`;
+
+export const Header = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+
+  z-index: 3;
+  background: var(--bg-color);
+
+  display: flex;
+  justify-content: space-between;
+
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 16px 32px;
+
+  > h1 {
+    display: flex;
+    align-items: center;
+
+    > span {
+      color: var(--text-color);
+      margin-left: 10px;
+      font-size: 29px;
+    }
+  }
+
+  > button {
+    color: var(--text-color);
+    background: none;
+    border: none;
+    outline: none;
+    font-size: 16px;
+    cursor: pointer;
+
+    &:hover,
+    &focus {
+      text-decoration: underline;
+    }
+  }
+`;
+
+export const DropboxLogo = styled(FaDropbox)`
+  width: 36px;
+  height: 32px;
+  fill: var(--logo-color);
+`;
+
+export const Content = styled.div`
+  z-index: 2;
+  position: relative;
+  max-width: 1440px;
+  margin: 0 auto;
+
+  > h2 {
+    font-size: var(--heading-font-size);
+    color: var(--logo-color);
+    max-width: var(--content-width);
+  }
+  > p {
+    margin-top: 20px;
+    font-size: 16px;
+    color: var(--text-color);
+    max-width: var(--content-width);
+  }
+
+  padding: var(--padding-top) 32px var(--padding-bottom);
+`;
